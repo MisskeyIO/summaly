210a0758e72331c42038afdd6c1f953122228a01
あわわわとーにゅ <17376330+u1-liquid@users.noreply.github.com>
2024-11-08 14:03:04 +0900

    distにタイプ定義を追加・corepack install (MisskeyIO#14)

---
+++ dist/utils/encoding.d.ts
@@ -0,0 +1,9 @@
+import type * as Got from 'got';
+/**
+ * Detect HTML encoding
+ * @param response Response
+ * @returns encoding
+ */
+export declare function detectEncoding(response: Got.Response<string>): string;
+export declare function toUtf8(body: Buffer, encoding: string): string;
+//# sourceMappingURL=encoding.d.ts.map
